4f0a3427bb09d4ec88740c4330e5b8f845530605
Manolis <manolis@kentik.com>
2025-06-14 23:17:31 +0300

    Site Built

---
+++ public/searchindex.en.js
@@ -0,0 +1,202 @@
+var relearn_searchindex = [
+  {
+    "breadcrumb": "Kentik - Hands On Lab \u003e  Lab.1 - Onboarding",
+    "content": "Step.1 - Title Step.2 - Title Step.3 - Title Step.4 - Title Step.5 - Title",
+    "description": "Deploy Universal Agent and Onboard Devices via Kentik NMS",
+    "tags": [],
+    "title": "Task 1.1 - Device Discovery",
+    "uri": "/lab-01/task_1/index.html"
+  },
+  {
+    "breadcrumb": "Kentik - Hands On Lab \u003e  Lab.2 - Alerting",
+    "content": "Step.1 - Title Step.2 - Title Step.3 - Title Step.4 - Title Step.5 - Title",
+    "description": "Short Description",
+    "tags": [],
+    "title": "Task 2.1 - NMS Alerting",
+    "uri": "/lab-02/task_1/index.html"
+  },
+  {
+    "breadcrumb": "Kentik - Hands On Lab \u003e  Lab.3 - Synthetics",
+    "content": "Step.1 - Title Step.2 - Title Step.3 - Title Step.4 - Title Step.5 - Title",
+    "description": "Short Description",
+    "tags": [],
+    "title": "Task 3.1 - Deploy Agents",
+    "uri": "/lab-03/task_1/index.html"
+  },
+  {
+    "breadcrumb": "Kentik - Hands On Lab \u003e  Lab.4 - Custom Metrics",
+    "content": "Step.1 - Title Step.2 - Title Step.3 - Title Step.4 - Title Step.5 - Title",
+    "description": "Short Description",
+    "tags": [],
+    "title": "Task 4.1 - Title",
+    "uri": "/lab-04/task_1/index.html"
+  },
+  {
+    "breadcrumb": "Kentik - Hands On Lab",
+    "content": "Prior to this workshop, you should have received an email with instructions on how to set up a free trial Kentik account. This is a prerequisite step in order to be able to run the Labs during the workshop.\nIf you already have registered for an account you can follow the tasks for validation.\nTask 0.1 - Register for a Free Trial Account Navigate to kentik.com/get-started and fill in your details in the form to create your free trial Kentik account.\nWarning Use your personal email and stick to the US portal. For the company field, you could specify something like partnerwks_\u003cnickname\u003e.\nOnce you submit the form, check your inbox for an account activation email and follow the link in order to set your password and activate your account.\nClick the Return to Kentik link or navigate to portal.kentik.com and log in using your credentials.\nTask 0.2 - Access the portal and check out the platform Once logged in for the first time, you can click the Next: Start Monitoring button to get access to the portal.\nClick on the hamburger icon on the top right corner to expose the Menu.\nHere you can access the different products and workflows available in the platform.\nClick on the Building icon on the top right corner to expose your Organization Settings and note your Account ID number assigned.\nClick the Licenses link to get to your licensing page.\nTip If you are redirected to the Onboarding Wizard page, just do a logout/login and visit your license page once more\nHere you can see the Free Licenses and their limits you can use in the respective products in the platform.\nThanks for Activating your trial account",
+    "description": "Prior to this workshop, you should have received an email with instructions on how to set up a free trial Kentik account. This is a prerequisite step in order to be able to run the Labs during the workshop.\nIf you already have registered for an account you can follow the tasks for validation.\nTask 0.1 - Register for a Free Trial Account Navigate to kentik.com/get-started and fill in your details in the form to create your free trial Kentik account.",
+    "tags": [],
+    "title": "Pre-Workshop Tasks",
+    "uri": "/pre-lab/index.html"
+  },
+  {
+    "breadcrumb": "Kentik - Hands On Lab \u003e  Lab.1 - Onboarding",
+    "content": "Step.1 - Title Step.2 - Title Step.3 - Title Step.4 - Title Step.5 - Title",
+    "description": "Configure relevant Kentik Resources to support our use case",
+    "tags": [],
+    "title": "Task 1.2 - Portal Configuration",
+    "uri": "/lab-01/task_2/index.html"
+  },
+  {
+    "breadcrumb": "Kentik - Hands On Lab \u003e  Lab.2 - Alerting",
+    "content": "Step.1 - Title Step.2 - Title Step.3 - Title Step.4 - Title Step.5 - Title",
+    "description": "Short Description",
+    "tags": [],
+    "title": "Task 2.2 - Traffic Alerting",
+    "uri": "/lab-02/task_2/index.html"
+  },
+  {
+    "breadcrumb": "Kentik - Hands On Lab \u003e  Lab.3 - Synthetics",
+    "content": "Step.1 - Title Step.2 - Title Step.3 - Title Step.4 - Title Step.5 - Title",
+    "description": "Short Description",
+    "tags": [],
+    "title": "Task 3.2 - Agent-to-Agent Tests",
+    "uri": "/lab-03/task_2/index.html"
+  },
+  {
+    "breadcrumb": "Kentik - Hands On Lab \u003e  Lab.4 - Custom Metrics",
+    "content": "Step.1 - Title Step.2 - Title Step.3 - Title Step.4 - Title Step.5 - Title",
+    "description": "Short Description",
+    "tags": [],
+    "title": "Task 4.2 - Title",
+    "uri": "/lab-04/task_2/index.html"
+  },
+  {
+    "breadcrumb": "Kentik - Hands On Lab",
+    "content": "This section will familiarize you with the network topology that we are going to be using throughout this workshop.\nWhile go through the topology, please, access the LAB link and start LAB.1 that will take about 5 minutes to load and set things up for us.",
+    "description": "This section will familiarize you with the network topology that we are going to be using throughout this workshop.\nWhile go through the topology, please, access the LAB link and start LAB.1 that will take about 5 minutes to load and set things up for us.",
+    "tags": [],
+    "title": "Topology",
+    "uri": "/topology/index.html"
+  },
+  {
+    "breadcrumb": "Kentik - Hands On Lab",
+    "content": "Theory and LAB description\nTask 1.1 - Device DiscoveryDeploy Universal Agent and Onboard Devices via Kentik NMS\nTask 1.2 - Portal ConfigurationConfigure relevant Kentik Resources to support our use case\nTask 1.3 - Enabling FlowPerform required steps to ingest netflow from devices\nTask 1.4 - Portal VerificationVerify visibility in the portal",
+    "description": "Theory and LAB description\nTask 1.1 - Device DiscoveryDeploy Universal Agent and Onboard Devices via Kentik NMS\nTask 1.2 - Portal ConfigurationConfigure relevant Kentik Resources to support our use case\nTask 1.3 - Enabling FlowPerform required steps to ingest netflow from devices\nTask 1.4 - Portal VerificationVerify visibility in the portal",
+    "tags": [],
+    "title": "Lab.1 - Onboarding",
+    "uri": "/lab-01/index.html"
+  },
+  {
+    "breadcrumb": "Kentik - Hands On Lab \u003e  Lab.1 - Onboarding",
+    "content": "Step.1 - Title Step.2 - Title Step.3 - Title Step.4 - Title Step.5 - Title",
+    "description": "Perform required steps to ingest netflow from devices",
+    "tags": [],
+    "title": "Task 1.3 - Enabling Flow",
+    "uri": "/lab-01/task_3/index.html"
+  },
+  {
+    "breadcrumb": "Kentik - Hands On Lab \u003e  Lab.2 - Alerting",
+    "content": "Step.1 - Title Step.2 - Title Step.3 - Title Step.4 - Title Step.5 - Title",
+    "description": "Short Description",
+    "tags": [],
+    "title": "Task 2.3 - DDoS Alerting",
+    "uri": "/lab-02/task_3/index.html"
+  },
+  {
+    "breadcrumb": "Kentik - Hands On Lab \u003e  Lab.3 - Synthetics",
+    "content": "Step.1 - Title Step.2 - Title Step.3 - Title Step.4 - Title Step.5 - Title",
+    "description": "Short Description",
+    "tags": [],
+    "title": "Task 3.3 - Title",
+    "uri": "/lab-03/task_3/index.html"
+  },
+  {
+    "breadcrumb": "Kentik - Hands On Lab \u003e  Lab.4 - Custom Metrics",
+    "content": "Step.1 - Title Step.2 - Title Step.3 - Title Step.4 - Title Step.5 - Title",
+    "description": "Short Description",
+    "tags": [],
+    "title": "Task 4.3 - Title",
+    "uri": "/lab-04/task_3/index.html"
+  },
+  {
+    "breadcrumb": "Kentik - Hands On Lab",
+    "content": "Theory and LAB description\nTask 2.1 - NMS AlertingShort Description\nTask 2.2 - Traffic AlertingShort Description\nTask 2.3 - DDoS AlertingShort Description\nTask 2.4 - TitleShort Description",
+    "description": "Theory and LAB description\nTask 2.1 - NMS AlertingShort Description\nTask 2.2 - Traffic AlertingShort Description\nTask 2.3 - DDoS AlertingShort Description\nTask 2.4 - TitleShort Description",
+    "tags": [],
+    "title": "Lab.2 - Alerting ",
+    "uri": "/lab-02/index.html"
+  },
+  {
+    "breadcrumb": "Kentik - Hands On Lab \u003e  Lab.1 - Onboarding",
+    "content": "Step.1 - Title Step.2 - Title Step.3 - Title Step.4 - Title Step.5 - Title",
+    "description": "Verify visibility in the portal",
+    "tags": [],
+    "title": "Task 1.4 - Portal Verification",
+    "uri": "/lab-01/task_4/index.html"
+  },
+  {
+    "breadcrumb": "Kentik - Hands On Lab \u003e  Lab.2 - Alerting",
+    "content": "Step.1 - Title Step.2 - Title Step.3 - Title Step.4 - Title Step.5 - Title",
+    "description": "Short Description",
+    "tags": [],
+    "title": "Task 2.4 - Title",
+    "uri": "/lab-02/task_4/index.html"
+  },
+  {
+    "breadcrumb": "Kentik - Hands On Lab \u003e  Lab.3 - Synthetics",
+    "content": "Step.1 - Title Step.2 - Title Step.3 - Title Step.4 - Title Step.5 - Title",
+    "description": "Short Description",
+    "tags": [],
+    "title": "Task 3.4 - Title",
+    "uri": "/lab-03/task_4/index.html"
+  },
+  {
+    "breadcrumb": "Kentik - Hands On Lab \u003e  Lab.4 - Custom Metrics",
+    "content": "Step.1 - Title Step.2 - Title Step.3 - Title Step.4 - Title Step.5 - Title",
+    "description": "Short Description",
+    "tags": [],
+    "title": "Task 4.4 - Title",
+    "uri": "/lab-04/task_4/index.html"
+  },
+  {
+    "breadcrumb": "Kentik - Hands On Lab",
+    "content": "Theory and LAB description\nTask 3.1 - Deploy AgentsShort Description\nTask 3.2 - Agent-to-Agent TestsShort Description\nTask 3.3 - TitleShort Description\nTask 3.4 - TitleShort Description",
+    "description": "Theory and LAB description\nTask 3.1 - Deploy AgentsShort Description\nTask 3.2 - Agent-to-Agent TestsShort Description\nTask 3.3 - TitleShort Description\nTask 3.4 - TitleShort Description",
+    "tags": [],
+    "title": "Lab.3 - Synthetics",
+    "uri": "/lab-03/index.html"
+  },
+  {
+    "breadcrumb": "Kentik - Hands On Lab",
+    "content": "Theory and LAB description\nTask 4.1 - TitleShort Description\nTask 4.2 - TitleShort Description\nTask 4.3 - TitleShort Description\nTask 4.4 - TitleShort Description",
+    "description": "Theory and LAB description\nTask 4.1 - TitleShort Description\nTask 4.2 - TitleShort Description\nTask 4.3 - TitleShort Description\nTask 4.4 - TitleShort Description",
+    "tags": [],
+    "title": "Lab.4 - Custom Metrics",
+    "uri": "/lab-04/index.html"
+  },
+  {
+    "breadcrumb": "",
+    "content": "A workshop towards getting the HOL Kentik picture",
+    "description": "A workshop towards getting the HOL Kentik picture",
+    "tags": [],
+    "title": "Kentik - Hands On Lab",
+    "uri": "/index.html"
+  },
+  {
+    "breadcrumb": "Kentik - Hands On Lab",
+    "content": "",
+    "description": "",
+    "tags": [],
+    "title": "Categories",
+    "uri": "/categories/index.html"
+  },
+  {
+    "breadcrumb": "Kentik - Hands On Lab",
+    "content": "",
+    "description": "",
+    "tags": [],
+    "title": "Tags",
+    "uri": "/tags/index.html"
+  }
+]
